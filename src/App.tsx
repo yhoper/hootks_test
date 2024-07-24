57d21cae543a1b6896a77290a67a45ee3d60b5a8
Mperez <>
2024-07-24 17:14:02 -0400

    useState

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const products = [
+    { title: "Col", isFruit: false, id: 1 },
+    { title: "Ajo", isFruit: false, id: 2 },
+    { title: "Manzana", isFruit: true, id: 3 },
+  ];
 
+  const listItems = products.map((product) => (
+    <li
+      key={product.id}
+      style={{ color: product.isFruit ? "magenta" : "darkgreen" }}
+    >
+      {product.title}
+    </li>
+  ));
+
+  const handleClick = () => setCount(count + 1);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ul>{listItems}</ul>
+      <hr />
+      <button onClick={handleClick}> Hazme click</button>
+      <>Has presionado el boton {count}</>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
